9adf65e3775e5fda9765cff0fa48f1ef6639a00e
szabolcsg128 <szabolcsg128@gmail.com>
2016-11-21 13:58:14 +0100

    login

---
+++ Dolgozok/Dolgozok/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Dolgozok
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.felhasznalonev_textBox = new System.Windows.Forms.TextBox();
+            this.jelszo_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(103, 179);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(75, 23);
+            this.login_button.TabIndex = 0;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Felhasznalonev";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jelszo";
+            // 
+            // felhasznalonev_textBox
+            // 
+            this.felhasznalonev_textBox.Location = new System.Drawing.Point(148, 56);
+            this.felhasznalonev_textBox.Name = "felhasznalonev_textBox";
+            this.felhasznalonev_textBox.Size = new System.Drawing.Size(100, 20);
+            this.felhasznalonev_textBox.TabIndex = 3;
+            // 
+            // jelszo_textBox
+            // 
+            this.jelszo_textBox.Location = new System.Drawing.Point(148, 112);
+            this.jelszo_textBox.Name = "jelszo_textBox";
+            this.jelszo_textBox.Size = new System.Drawing.Size(100, 20);
+            this.jelszo_textBox.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.jelszo_textBox);
+            this.Controls.Add(this.felhasznalonev_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login_button);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox felhasznalonev_textBox;
+        private System.Windows.Forms.TextBox jelszo_textBox;
+    }
+}
---
+++ Dolgozok/Dolgozok/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+
+namespace Dolgozok
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            {
+
+            }
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            Adatbazis db = Adatbazis.GetPeldany();
+
+            do
+            {
+                SQLiteDataReader reader = db.Lekerdezes("select * from Dolgozok where Felh_nev =" + felhasznalonev_textBox);
+                if (jelszo_textBox.Equals(reader.GetValue(9)))
+                {
+                    this.Hide();
+                    Form1 user = new Form1();
+                    user.Show();
+                }
+            } while (false);
+
+        }
+    }
+}
